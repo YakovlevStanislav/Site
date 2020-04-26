df3249937270b38e4bab7f49cc85c575f23751b2
Stanislav <stas-68@mail.ru>
2020-04-26 11:21:57 +0300

    Задание 1.2

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, img = "https://unsplash.it/150/150?random&amp;gravity=3") => {
   return `<div class="product-item">
             <h3>${title}</h3>
+            <img src=${img}>
             <p>${price}</p>
             <button class="by-btn">Добавить в корзину</button>
           </div>`;
