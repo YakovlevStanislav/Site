31affeec0e5b0fb7e0ac571d06e90f2228267d5d
Stanislav <stas-68@mail.ru>
2020-04-30 09:28:12 +0300

    задание 3.1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать в ДЗ
-let getRequest = (url, cb) => {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status !== 200) {
-        console.log('Error');
-      } else {
-        cb(xhr.responseText);
-      }
-    }
-  };
-  xhr.send();
-};
+let getRequest = (`${API}/getBasket.json`)
+.then(response => response.json())
+.catch(error => {
+  console.log(error);
+});
+
 
 class ProductList {
   constructor(container = '.products') {
